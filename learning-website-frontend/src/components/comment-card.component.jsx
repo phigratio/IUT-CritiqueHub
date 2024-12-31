23a43322e8866f788eb6b7f8fbbd8bb37fc8452e
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-12-31 17:17:04 +0600

    hide and show reply

--- learning-website-frontend/src/components/comment-card.component.jsx
+++ learning-website-frontend/src/components/comment-card.component.jsx
@@ -3,6 +3,8 @@ import {UserContext} from "../App";
 import { getDay } from "../common/date";
 import toast from "react-hot-toast";
 import CommentField from "./comment-field.component";
+import { BlogContext } from "../pages/blog.page";
+import axios from "axios";
 
 const CommentCard = ({ index, leftVal, commentData }) => {
   let {
@@ -11,12 +13,51 @@ const CommentCard = ({ index, leftVal, commentData }) => {
     },
     commentedAt,
     comment,
-    _id,
+    _id,children
   } = commentData;
+
+  let{blog,blog:{comments,comments:{results:commentsArr}},setBlog}=useContext(BlogContext)
 
   let {userAuth:{access_token}}=useContext(UserContext);
 
   const [isReplying,setReplying]=useState(false);
+
+  const removeCommentsCards=(startingPoint)=>{
+    if(commentsArr[startingPoint]){
+      while(commentsArr[startingPoint].childrenLevel>commentData.childrenLevel){
+        commentsArr.splice(startingPoint,1)
+
+        if(!commentsArr[startingPoint]){
+          break;
+        }
+      }
+    }
+    setBlog({...blog,comments:{results:commentsArr}})
+  }
+
+  const loadReplies=({skip=0})=>{
+    if(children.length){
+      hideReplies();
+      axios.post(import.meta.env.VITE_SERVER_DOMAIN+"/get-replies",{_id,skip})
+      .then(({data:{replies}})=>{
+        commentData.isReplyLoaded=true;
+        for(let i=0;i<replies.length;i++){
+          replies[i].childrenLevel=commentData.childrenLevel+1;
+          commentsArr.splice(index+1+i+skip,0,replies[i])
+        }
+        setBlog({...blog,comments:{...comments,results:commentsArr}})
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+
+  }
+
+  const hideReplies=()=>{
+    commentData.isReplyLoaded=false;
+    removeCommentsCards(index+1)
+  }
 
     const handleReplyClick = () => {
         if(!access_token){
@@ -38,6 +79,19 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         <p className="font-gelasio text-xl ml-3">{comment}</p>
 
         <div className="flex gap-5 items-center mt-5">
+          {
+            commentData.isReplyLoaded ?
+            <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={hideReplies}>
+              <i class="bi bi-chat-dots-fill"></i>
+              Hide Reply
+
+            </button> : 
+            <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={loadReplies}>
+               <i class="bi bi-chat-dots-fill"></i>
+               {children.length} Reply
+
+            </button>
+          }
           <button className="underline" onClick={handleReplyClick}>Reply</button>
         </div>
         {
--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ import firebaseConfig from "./firebase.config.js";
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
 import Notification from "./Schema/Notification.js";
+import { populate } from "dotenv";
 
 const server = express();
 
@@ -613,6 +614,7 @@ server.post("/add-comment", verifyJWT, (req, res) => {
 
   if(replying_to){
     commentObj.parent=replying_to;
+    commentObj.isReply=true;
   }
   new Comment(commentObj).save().then(async commentFile=>{
     let {comment,commentedAt,children}=commentFile;
@@ -656,6 +658,33 @@ server.post("/get-blog-comments",(req,res)=>{
   })
   .catch(err=>{
     console.log(err.message);
+    return res.status(500).json({error:err.message})
+  })
+})
+
+server.post("/get-replies",(req,res)=>{
+  let{_id,skip}=req.body;
+  let maxLimit=5;
+  Comment.findOne({_id})
+  .populate({
+    path:"children",
+    option:{
+      limit:maxLimit,
+      skip:skip,
+      sort:{'commentedAt':-1}
+    },
+    populate:{
+      path:'commented_by',
+      select:"personal_info.profile_img personal_info.fullname personal_info.username"
+    },
+    select:"-blog_id -updatedAt"
+
+  })
+  .select("children")
+  .then(doc=>{
+    return res.status(200).json({replies:doc.children})
+  })
+  .catch(err=>{
     return res.status(500).json({error:err.message})
   })
 })
