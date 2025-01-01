bf51b7ab7ea03eade4a7cf9eb725fbdc0802d99d
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2025-01-01 10:42:07 +0600

    delete comment(playlist completed)

--- learning-website-frontend/src/components/comment-card.component.jsx
+++ learning-website-frontend/src/components/comment-card.component.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import {UserContext} from "../App";
+import { UserContext } from "../App";
 import { getDay } from "../common/date";
 import toast from "react-hot-toast";
 import CommentField from "./comment-field.component";
@@ -9,97 +9,183 @@ import axios from "axios";
 const CommentCard = ({ index, leftVal, commentData }) => {
   let {
     commented_by: {
-      personal_info: { profile_img, fullname, username },
+      personal_info: { profile_img, fullname, username: commented_by_username },
     },
     commentedAt,
     comment,
-    _id,children
+    _id,
+    children,
   } = commentData;
 
-  let{blog,blog:{comments,comments:{results:commentsArr}},setBlog}=useContext(BlogContext)
+  let {
+    blog,
+    blog: {
+      comments, activity,activity:{total_parent_comments},
+      comments: { results: commentsArr },
+      author:{personal_info:{username:blog_author}}
+    },
+    setBlog,setTotalParentCommentsLoaded
+  } = useContext(BlogContext);
 
-  let {userAuth:{access_token}}=useContext(UserContext);
+  let {
+    userAuth: { access_token, username },
+  } = useContext(UserContext);
 
-  const [isReplying,setReplying]=useState(false);
+  const [isReplying, setReplying] = useState(false);
 
-  const removeCommentsCards=(startingPoint)=>{
-    if(commentsArr[startingPoint]){
-      while(commentsArr[startingPoint].childrenLevel>commentData.childrenLevel){
-        commentsArr.splice(startingPoint,1)
+  const getParentIndex = () => {
+    let startingPoint = index - 1;
 
-        if(!commentsArr[startingPoint]){
+    try{
+      while(commentsArr[startingPoint].childrenLevel >= commentData.childrenLevel){
+        startingPoint--;
+      }
+    } catch{
+      startingPoint=undefined;
+    }
+    return startingPoint;
+  }
+
+  const removeCommentsCards = (startingPoint,isDelete=false) => {
+    if (commentsArr[startingPoint]) {
+      while (
+        commentsArr[startingPoint].childrenLevel > commentData.childrenLevel
+      ) {
+        commentsArr.splice(startingPoint, 1);
+
+        if (!commentsArr[startingPoint]) {
           break;
         }
       }
     }
-    setBlog({...blog,comments:{results:commentsArr}})
-  }
 
-  const loadReplies=({skip=0})=>{
-    if(children.length){
+    if(isDelete){
+      let parentIndex=getParentIndex();
+
+      if(parentIndex!=undefined){
+        commentsArr[parentIndex].children=commentsArr[parentIndex].children.filter(child=>child!=_id);
+
+        if(!commentsArr[parentIndex].children.length){
+          commentsArr[parentIndex].isReplyLoaded=false;
+        }
+
+
+      }
+      commentsArr.splice(index, 1);
+    }
+    if(commentData.childrenLevel==0 && isDelete){
+      setTotalParentCommentsLoaded(preVal=>preVal-1);
+    }
+    setBlog({ ...blog, comments: { results: commentsArr },activity:{...activity,total_parent_comments:total_parent_comments-(commentData.childrenLevel== 0 && isDelete ? 1:0)} });
+  };
+
+  const loadReplies = ({ skip = 0 }) => {
+    if (children.length) {
       hideReplies();
-      axios.post(import.meta.env.VITE_SERVER_DOMAIN+"/get-replies",{_id,skip})
-      .then(({data:{replies}})=>{
-        commentData.isReplyLoaded=true;
-        for(let i=0;i<replies.length;i++){
-          replies[i].childrenLevel=commentData.childrenLevel+1;
-          commentsArr.splice(index+1+i+skip,0,replies[i])
-        }
-        setBlog({...blog,comments:{...comments,results:commentsArr}})
-      })
-      .catch(err=>{
-        console.log(err);
-      })
+      axios
+        .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", {
+          _id,
+          skip,
+        })
+        .then(({ data: { replies } }) => {
+          commentData.isReplyLoaded = true;
+          for (let i = 0; i < replies.length; i++) {
+            replies[i].childrenLevel = commentData.childrenLevel + 1;
+            commentsArr.splice(index + 1 + i + skip, 0, replies[i]);
+          }
+          setBlog({ ...blog, comments: { ...comments, results: commentsArr } });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 
-  }
-
-  const hideReplies=()=>{
-    commentData.isReplyLoaded=false;
-    removeCommentsCards(index+1)
-  }
-
-    const handleReplyClick = () => {
-        if(!access_token){
-            return toast.error("login first to leave a reply");
+  const deleteComment = (e) => {
+    e.target.setAttribute("disabled", true);
+    axios
+      .post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/delete-comment",
+        { _id },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
         }
-        setReplying(preVal=>!preVal);
+      )
+      .then(() => {
+        e.target.removeAttribute("disabled");
+        removeCommentsCards(index+1,true);
+      });
+  };
 
+  const hideReplies = () => {
+    commentData.isReplyLoaded = false;
+    removeCommentsCards(index + 1);
+  };
+
+  const handleReplyClick = () => {
+    if (!access_token) {
+      return toast.error("login first to leave a reply");
     }
+    setReplying((preVal) => !preVal);
+  };
   return (
     <div className="w-full" style={{ paddingLeft: `${leftVal * 10}px` }}>
       <div className="my-5 p-6 rounded-md border border-grey">
         <div className="flex gap-3 items-center mb-8">
           <img src={profile_img} className="w-6 h-6 rounded-full" />
           <p className="line-clamp-1">
-            {fullname} @{username}
+            {fullname} @{commented_by_username}
           </p>
           <p className="min-w-fit">{getDay(commentedAt)}</p>
         </div>
         <p className="font-gelasio text-xl ml-3">{comment}</p>
 
         <div className="flex gap-5 items-center mt-5">
-          {
-            commentData.isReplyLoaded ?
-            <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={hideReplies}>
+          {commentData.isReplyLoaded ? (
+            <button
+              className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+              onClick={hideReplies}
+            >
               <i class="bi bi-chat-dots-fill"></i>
               Hide Reply
-
-            </button> : 
-            <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2" onClick={loadReplies}>
-               <i class="bi bi-chat-dots-fill"></i>
-               {children.length} Reply
-
             </button>
-          }
-          <button className="underline" onClick={handleReplyClick}>Reply</button>
+          ) : (
+            <button
+              className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+              onClick={loadReplies}
+            >
+              <i class="bi bi-chat-dots-fill"></i>
+              {children.length} Reply
+            </button>
+          )}
+          <button className="underline" onClick={handleReplyClick}>
+            Reply
+          </button>
+          {username == commented_by_username || username == blog_author ? (
+            <button
+              className="p-2 px-3 rounded-md border border-grey ml-auto hover:bg-red/30 hover:text-red flex items-center"
+              onClick={deleteComment}
+            >
+              <i class="bi bi-trash3 pointer-events-none"></i>
+            </button>
+          ) : (
+            ""
+          )}
         </div>
-        {
-            isReplying?
-            <div className="mt-8">
-                <CommentField action="reply" index={index} replyingTo={_id} setReplying={setReplying}/>
-            </div>:""
-        }
+        {isReplying ? (
+          <div className="mt-8">
+            <CommentField
+              action="reply"
+              index={index}
+              replyingTo={_id}
+              setReplying={setReplying}
+            />
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
